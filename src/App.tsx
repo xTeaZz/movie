1ff225c428a5d7c13eeec123bd9b05f5c0de9f6e
Jonathan Ballauri <jonathanballauri@gmail.com>
2022-09-26 15:52:47 +0900

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Login />
       </header>
     </div>
   );
